d354714869e55df26f700a7691cd053a1f4c0971
Erinna Chen <erinna.chen@gmail.com>
2016-04-18 09:17:04 -0600

    Add user can location stations by zip feature
    * Added SearchController
    * Added view for action search#index
    * Feature test is only partially written

---
+++ spec/features/user_can_locate_stations_by_zip_spec.rb
@@ -0,0 +1,22 @@
+# As a user
+# When I visit "/"
+# And I fill in the search form with 80203
+# And I click "Locate"
+# Then I should be on page "/search?zip=80203"
+# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+# And the stations should be limited to Electric and Propane
+# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+
+require "rails_helper"
+
+RSpec.feature "User can find stations by zip code" do
+  scenario "see stations for electrip and propane within 6 miles with info" do
+    #VCR.use_cassette "search_by_zipcode" do
+    visit '/'
+    fill_in "q", with: "80203"
+    click_on "Locate"
+
+    expect(current_path).to eq "/search?zip=80203"
+    #end
+  end
+end
